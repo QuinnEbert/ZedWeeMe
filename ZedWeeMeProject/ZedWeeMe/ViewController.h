24139277c22d0c56cfa8f7ba652b4b494a8fc48d
Quinn Ebert <quinn@quinnebert.net>
2014-02-05 05:13:03 -0500

    Whole tonne of changes/additions!
    + Fix crappy bottom of UI appearance on iPhone5/5C/5S.
    + Added a new first step of the robot activity: it will orient the robot to 0 degrees based on the phone's orientation (beginnings of experimenting with something approaching the ability to discover its environment perhaps).  Once it's done orienting the usual old stuff will commence.
    + Adjusted down the loudspeaker volume (so it doesn't sound horrifically distorted with the RoboMe's bad DAC/speaker).
    + Cleaned up a crap ton of old noisy NSLog stuff in my own code (there's still a lot from RoboMe SDK I can't do anything about sadly).
    + Cleaned out old disused UI elements (and related event functions) from the code.
    + Added full-head-tilt-up to the app start up procedure (useful when RoboMe exoskel is on your desk with your desktop and you need to read the cradled device's display). :-)
    + Disabled some old TTS/battery related functions that weren't really used anymore.
    + Improve information regarding exoskeleton battery level (textual).
    + Remove the Flite/SLT stuff (maybe it will help improve startup times, just maybe).
    + Add web-based logging (easier than always having to follow the damn thing around with an attached dock/Lightning cable) -- disabled by default, script available later -- or you should be able to roll your own with one line of PHP. ;-)
    + Cleaned up a few compiler warnings (I think I did this anyway).

--- ZedWeeMeProject/ZedWeeMe/ViewController.h
+++ ZedWeeMeProject/ZedWeeMe/ViewController.h
@@ -7,24 +7,35 @@
 
 #import <UIKit/UIKit.h>
 #import <RoboMe/RoboMe.h>
-#import <Slt/Slt.h>
+//#import <Slt/Slt.h>
+#import <CoreLocation/CoreLocation.h>
 
-#define TURNBACK_MAX 2
+#define TURNBACK_MAX   2
 
-@class FliteController;
+#define COMPASS_ORIENT_LO   (  0.0+(15.0/2.0))
+#define COMPASS_ORIENT_HI   (360.0-(15.0/2.0))
 
-@interface ViewController : UIViewController <RoboMeDelegate> {
-    Slt *slt;
-    FliteController *fliteController;
+#define DO_WEB_BASED_LOGS   0
+
+/*@class FliteController;*/
+
+@interface ViewController : UIViewController <RoboMeDelegate,CLLocationManagerDelegate> {
+    //Slt *slt;
+    //FliteController *fliteController;
     int batterySpeechCt;
     bool cm020;
     bool cm050;
     bool cm100;
     bool turning;
+    bool logging;
     int turnDir;
     int turnback_cur;
     NSTimer *aTimer;
     NSTimer *bTimer;
+    CLLocationManager *locationMgr;
+
+    double compass;
+    bool orienting;
 }
 
 @property (weak, nonatomic) IBOutlet UITextView *outputTextView;
@@ -35,16 +46,21 @@
 @property (weak, nonatomic) IBOutlet UILabel *cheat100cmLabel;
 
 @property (nonatomic, strong) AVAudioPlayer *aPlayer;
+@property (nonatomic, retain) CLLocationManager *locationMgr;
 
 @property (nonatomic) int batterySpeechCt;
 @property (nonatomic) bool cm020;
 @property (nonatomic) bool cm050;
 @property (nonatomic) bool cm100;
 @property (nonatomic) bool turning;
+@property (nonatomic) bool orienting;
+@property (nonatomic) bool logging;
 @property (nonatomic) int turnDir;
 @property (nonatomic) int turnback_cur;
 
-@property (nonatomic, strong) Slt *slt;
-@property (nonatomic, strong) FliteController *fliteController;
+@property (nonatomic) double compass;
+
+//@property (nonatomic, strong) Slt *slt;
+//@property (nonatomic, strong) FliteController *fliteController;
 
 @end
--- ZedWeeMeProject/ZedWeeMe/ViewController.mm
+++ ZedWeeMeProject/ZedWeeMe/ViewController.mm
@@ -7,7 +7,8 @@
 
 #import "ViewController.h"
 
-#import <OpenEars/FliteController.h>
+//#import <OpenEars/FliteController.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface ViewController ()
 
@@ -17,8 +18,8 @@
 
 @implementation ViewController 
 
-@synthesize slt;
-@synthesize fliteController;
+//@synthesize slt;
+//@synthesize fliteController;
 @synthesize batterySpeechCt;
 @synthesize cm020;
 @synthesize cm050;
@@ -29,8 +30,16 @@
 
 @synthesize turnback_cur;
 
+@synthesize logging;
+
+@synthesize locationMgr;
+@synthesize compass;
+@synthesize orienting;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self logHollyAnn:@"********************** SYSTEM RESET **********************"];
     
     cm020 = NO;
     cm050 = NO;
@@ -38,6 +47,11 @@
     
     turning = NO;
     turnDir = 0;
+    
+    logging = NO;
+    
+    compass = 0.0;
+    orienting = NO;
     
     // create RoboMe object
     self.roboMe = [[RoboMe alloc] initWithDelegate: self];
@@ -49,6 +63,115 @@
     
     // set the speech squelch:
     self.batterySpeechCt = 0;
+}
+
+- (IBAction)test_run:(id)sender {
+    [self.roboMe sendCommand:kRobot_HeadTiltAllDown];
+    self.orienting = YES;
+    aTimer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
+}
+
+-(bool)randomChance
+{
+    int val = arc4random_uniform(2);
+    //NSLog(@"Got random value: %i",val);
+    if (val>0)
+        return YES;
+    return NO;
+}
+
+-(void)startDelay:(NSTimer *) theTimer
+{
+    [self.roboMe sendCommand:kRobot_HeadTiltAllUp];
+    [self.roboMe sendCommand:kRobot_GetBatteryLevel];
+}
+
+-(void)timerFired:(NSTimer *) theTimer
+{
+    if (!logging) {
+        if (self.orienting) {
+            if (self.compass>=COMPASS_ORIENT_HI||self.compass<=COMPASS_ORIENT_LO) {
+                [self.roboMe sendCommand:kRobot_Stop];
+                self.orienting = NO;
+                [self logHollyAnn:@"0deg compass orientation completed!!!"];
+            } else {
+                [self.roboMe sendCommand:kRobot_TurnRightSlowest];
+                [self logHollyAnn:[NSString stringWithFormat:@"0deg orientation: compass reads %fdg bearing, twitch right", self.compass]];
+            }
+        } else {
+            if (turning && turnback_cur <= TURNBACK_MAX/2) {
+                [self.roboMe sendCommand:kRobot_MoveBackward];
+                turnback_cur--;
+                if (turnback_cur < 0) {
+                    [self logHollyAnn:@"Just finished turning back!"];
+                    turnback_cur = 0;
+                    turning = NO;
+                } else {
+                    [self logHollyAnn:@"In the midst of turning back!"];
+                }
+            } else {
+                if (!self.cm050) {
+                    turning = NO;
+                    turnDir = 0;
+                    //NSLog(@"Empty at 0.5metre");
+                    [self logHollyAnn:@"Nothing 0.5 metres away, move forward!"];
+                    [self.roboMe sendCommand:kRobot_MoveForward];
+                } else {
+                    //NSLog(@"Stuff at 0.5metre");
+                    [self logHollyAnn:@"Detected object 0.5 metres away..."];
+                    if (!turning) {
+                        turnback_cur = TURNBACK_MAX;
+                        turning = YES;
+                        bool r = [self randomChance];
+                        if (!r) {
+                            [self logHollyAnn:@" >> Wasn't turning back, will begin turning left!"];
+                            [self.roboMe sendCommand:kRobot_TurnLeft];
+                            turnDir = 0;
+                        } else {
+                            [self logHollyAnn:@" >> Wasn't turning back, will begin turning left!"];
+                            [self.roboMe sendCommand:kRobot_TurnRight];
+                            turnDir = 1;
+                        }
+                    } else {
+                        [self logHollyAnn:@" >> Was turning back, resuming with previous direction!"];
+                        if (turnDir==0) {
+                            [self.roboMe sendCommand:kRobot_TurnLeft];
+                        } else {
+                            [self.roboMe sendCommand:kRobot_TurnRight];
+                        }
+                        turnback_cur--;
+                    }
+                }
+            }
+        }
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"Location manager: Oh $#@%%!");
+}
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    //NSLog(@"Got updated device location!");
+    
+    //TODO: experimentations?!?
+}
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
+{
+    double heading = [newHeading trueHeading];
+    NSLog(@"Updated device heading: %fdg",heading);
+    self.compass = heading;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    /*self.slt = [[Slt alloc] init];
+    self.fliteController = [[FliteController alloc] init];
+    self.fliteController.duration_stretch = .9; // Change the speed
+	self.fliteController.target_mean = 1.2; // Change the pitch
+	self.fliteController.target_stddev = 1.5; // Change the variance*/
     
     // battery level of the iOS device
     bool doBatMon = [UIDevice currentDevice].batteryMonitoringEnabled;
@@ -57,84 +180,52 @@
     [[UIDevice currentDevice] setBatteryMonitoringEnabled:doBatMon];
     NSString *batteryLevelTextInfo = [NSString stringWithFormat:@"iOS device battery level: %i percent",(int)(batteryLevel*100)];
     [self displayText:batteryLevelTextInfo];
-}
-
-- (IBAction)test_run:(id)sender {
-    [self.roboMe sendCommand:kRobot_HeadTiltAllDown];
-    aTimer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
-}
-
--(bool) randomChance
-{
-    int val = arc4random_uniform(2);
-    NSLog(@"Got random value: %i",val);
-    if (val>0)
-        return YES;
-    return NO;
-}
-
--(void)startDelay:(NSTimer *) theTimer
-{
-    [self.roboMe sendCommand:kRobot_GetBatteryLevel];
-}
-
--(void)timerFired:(NSTimer *) theTimer
-{
-    if (turning && turnback_cur <= TURNBACK_MAX/2) {
-        [self.roboMe sendCommand:kRobot_MoveBackward];
-        turnback_cur--;
-        if (turnback_cur < 0) {
-            turnback_cur = 0;
-            turning = NO;
-        }
-    } else {
-        if (!self.cm050) {
-            turning = NO;
-            turnDir = 0;
-            NSLog(@"Empty at 0.5metre");
-            [self.roboMe sendCommand:kRobot_MoveForward];
-        } else {
-            NSLog(@"Stuff at 0.5metre");
-            if (!turning) {
-                turnback_cur = TURNBACK_MAX;
-                turning = YES;
-                bool r = [self randomChance];
-                if (!r) {
-                    [self.roboMe sendCommand:kRobot_TurnLeft];
-                    turnDir = 0;
-                } else {
-                    [self.roboMe sendCommand:kRobot_TurnRight];
-                    turnDir = 1;
-                }
-            } else {
-                if (turnDir==0) {
-                    [self.roboMe sendCommand:kRobot_TurnLeft];
-                } else {
-                    [self.roboMe sendCommand:kRobot_TurnRight];
-                }
-                turnback_cur--;
-            }
-        }
-    }
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    
-    self.slt = [[Slt alloc] init];
-    self.fliteController = [[FliteController alloc] init];
-    self.fliteController.duration_stretch = .9; // Change the speed
-	self.fliteController.target_mean = 1.2; // Change the pitch
-	self.fliteController.target_stddev = 1.5; // Change the variance
     
     self.aPlayer = [[AVAudioPlayer alloc] init];
     [self playAudio:@"ready_robot"];
     bTimer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(startDelay:) userInfo:nil repeats:NO];
+    
+    /*dispatch_async(dispatch_get_main_queue(),^ {*/
+        self.locationMgr = [[CLLocationManager alloc] init];
+        self.locationMgr.desiredAccuracy = kCLLocationAccuracyBestForNavigation;
+        self.locationMgr.delegate = self;
+        self.locationMgr.distanceFilter = 0.1f;
+        [self.locationMgr startUpdatingLocation];
+        [self.locationMgr startUpdatingHeading];
+    /*} );*/
 }
 
 - (void)playAudio: (NSString*)file {
     self.aPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL URLWithString:[[NSBundle mainBundle] pathForResource:file ofType:@"aiff"]] error:nil];
+    [self.aPlayer setVolume:0.50];
     [self.aPlayer play];
+}
+
+- (NSString *)urlEncode: (NSString *)str {
+    CFStringRef safeString = CFURLCreateStringByAddingPercentEscapes (
+                                                                      NULL,
+                                                                      (CFStringRef)str,
+                                                                      NULL,
+                                                                      CFSTR("/%&=?$#+-~@<>|\\*,.()[]{}^!"),
+                                                                      kCFStringEncodingUTF8
+                                                                      );
+    NSString *r = (__bridge NSString *)safeString;
+    CFRelease(safeString);
+    return r;
+}
+
+- (void)logHollyAnn: (NSString *)msg {
+    self.logging = YES;
+    if (DO_WEB_BASED_LOGS) {
+        NSString *msgFull = [NSString stringWithFormat:@"%@",[[msg stringByReplacingOccurrencesOfString:@"\n" withString:@""] stringByReplacingOccurrencesOfString:@"\r" withString:@""]];
+        NSString *m = [NSString stringWithFormat:@"http://192.168.1.25/~quinn/weeloggy/?msg=%@",[self urlEncode:msgFull]];
+        NSURL *u = [[NSURL alloc] initWithString:m];
+        NSURLRequest *r = [[NSURLRequest alloc] initWithURL:u];
+        [NSURLConnection sendSynchronousRequest:r returningResponse:nil error:nil];
+        //NSString *d = [[NSString alloc] initWithData:notUsed encoding:NSUTF8StringEncoding];
+        //NSLog(@" ##retData## : %@",d);
+    }
+    self.logging = NO;
 }
 
 // Print out given text to text view
@@ -147,30 +238,31 @@
     // scroll to bottom
     [self.outputTextView scrollRangeToVisible:NSMakeRange([self.outputTextView.text length], 0)];
     
-    //NSLog(@" ## %@",outputTxt);
+    //NSLog(@" ##logHollyAnn## %@",outputTxt);
+    [self logHollyAnn:outputTxt];
 }
 
-#pragma mark - TTS Helpers
+/*** #pragma mark - TTS Helpers ***/
 
-- (void) say:(NSString *)message {
+/*- (void) say:(NSString *)message {
     [self.fliteController say:message withVoice:self.slt];
-}
+}*/
 
-- (void) batterySay:(NSString *)message {
+/*- (void) batterySay:(NSString *)message {
     if (! self.batterySpeechCt) {
         [self say:message];
         self.batterySpeechCt = (int)((300*1000)/(60*1000));
     } else {
         self.batterySpeechCt--;
     }
-}
+}*/
 
 #pragma mark - RoboMeConnectionDelegate
 
 // Event commands received from RoboMe
 - (void)commandReceived:(IncomingRobotCommand)command {
     // Display incoming robot command in text view
-    [self displayText: [NSString stringWithFormat: @"Received: %@" ,[RoboMeCommandHelper incomingRobotCommandToString: command]]];
+    //[self displayText: [NSString stringWithFormat: @"Received: %@" ,[RoboMeCommandHelper incomingRobotCommandToString: command]]];
     
     // To check the type of command from RoboMe is a sensor status use the RoboMeCommandHelper class
     if([RoboMeCommandHelper isSensorStatus:command]){
@@ -178,32 +270,39 @@
         SensorStatus *sensors = [RoboMeCommandHelper readSensorStatus: command];
         
         // Update labels
-        [self.edgeLabel setText: (sensors.edge ? @"ON" : @"OFF")];
+        //[self.edgeLabel setText: (sensors.edge ? @"ON" : @"OFF")];
         self.cm020 = sensors.chest_20cm ? YES : NO;
-        [self.chest20cmLabel setText: (sensors.chest_20cm ? @"ON" : @"OFF")];
+        //[self.chest20cmLabel setText: (sensors.chest_20cm ? @"ON" : @"OFF")];
         self.cm050 = sensors.chest_50cm ? YES : NO;
-        [self.chest50cmLabel setText: (sensors.chest_50cm ? @"ON" : @"OFF")];
+        //[self.chest50cmLabel setText: (sensors.chest_50cm ? @"ON" : @"OFF")];
         self.cm100 = sensors.chest_100cm ? YES : NO;
-        [self.cheat100cmLabel setText: (sensors.chest_100cm ? @"ON" : @"OFF")];
+        //[self.cheat100cmLabel setText: (sensors.chest_100cm ? @"ON" : @"OFF")];
     } else if ([RoboMeCommandHelper isBatteryStatus:command]) {
         // Read the battery status
         if (command==kRobotIncoming_Battery100) {
-            NSLog(@"Battery level reached 100 percent");
+            //NSLog(@"Battery level reached 100 percent");
+            [self displayText:@"Exoskeleton battery is full (100 percent)"];
             [self playAudio:@"battery_100"];
         } else if (command==kRobotIncoming_Battery80) {
-            NSLog(@"Battery level reached 80 percent");
+            //NSLog(@"Battery level reached 80 percent");
+            [self displayText:@"Exoskeleton batteries are strong (80 percent)"];
             [self playAudio:@"battery_080"];
         } else if (command==kRobotIncoming_Battery60) {
-            NSLog(@"Battery level reached 60 percent");
+            //NSLog(@"Battery level reached 60 percent");
+            [self displayText:@"Exoskeleton batteries are good (60 percent)"];
             [self playAudio:@"battery_060"];
         } else if (command==kRobotIncoming_Battery40) {
-            NSLog(@"Battery level reached 40 percent");
+            //NSLog(@"Battery level reached 40 percent");
+            [self displayText:@"Exoskeleton batteries are fair (40 percent)"];
             [self playAudio:@"battery_040"];
         } else if (command==kRobotIncoming_Battery20) {
-            NSLog(@"Battery level reached 20 percent");
+            //NSLog(@"Battery level reached 20 percent");
+            [self displayText:@"Exoskeleton batteries are weak (20 percent)"];
             [self playAudio:@"tired_robot"];
         } else if (command==kRobotIncoming_Battery10) {
-            NSLog(@"Battery level reached 10 percent");
+            //NSLog(@"Battery level reached 10 percent");
+            [self displayText:@"Exoskeleton batteries are nearly-exhausted (10 percent)"];
+            [self displayText:@"WARNING: Exoskeleton may operate too slowly on nearly-exhausted batteries!"];
             [self playAudio:@"tired_robot"];
         }
     }
@@ -222,39 +321,6 @@
 
 - (void)roboMeDisconnected {
     [self displayText: @"RoboMe Disconnected"];
-}
- 
-#pragma mark - Button callbacks
-
-// The methods below send the desired command to RoboMe.
-// Typically you would want to start a timer to repeatly send the
-// command while the button is held down. For simplicity this wasn't
-// included however if you do decide to implement this we recommand
-// sending commands every 500ms for smooth movement.
-// See RoboMeCommandHelper.h for a full list of robot commands
-- (IBAction)moveForwardBtnPressed:(UIButton *)sender {
-    // Adds command to the queue to send to the robot
-    [self.roboMe sendCommand: kRobot_MoveForwardFastest];
-}
-
-- (IBAction)moveBackwardBtnPressed:(UIButton *)sender {
-    [self.roboMe sendCommand: kRobot_MoveBackwardFastest];
-}
-
-- (IBAction)turnLeftBtnPressed:(UIButton *)sender {
-    [self.roboMe sendCommand: kRobot_TurnLeftFastest];
-}
-
-- (IBAction)turnRightBtnPressed:(UIButton *)sender {
-    [self.roboMe sendCommand: kRobot_TurnRightFastest];
-}
-
-- (IBAction)headUpBtnPressed:(UIButton *)sender {
-    [self.roboMe sendCommand: kRobot_HeadTiltAllUp];
-}
-
-- (IBAction)headDownBtnPressed:(UIButton *)sender {
-    [self.roboMe sendCommand: kRobot_HeadTiltAllDown];
 }
 
 @end
